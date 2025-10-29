b03c3e6e28ed7bdf53805de0f79e485d989873bd
LAB13_USER ON WS13 <L13X11W13@LabHCI.net>
2025-10-29 13:00:01 +0800

    Deployment

--- GradeNest/settings.py
+++ GradeNest/settings.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-test-key")
-DEBUG = True
-ALLOWED_HOSTS = ["*"]
+SECRET_KEY = env('DJANGO_SECRET_KEY')
+DEBUG = env('DEBUG', default=True)
 
+ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
