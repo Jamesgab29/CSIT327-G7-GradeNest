718c72b2ae5182a42eabb55881a31d31c8ebcdfa
LAB13_USER ON WS13 <L13X11W13@LabHCI.net>
2025-10-29 12:08:23 +0800

    setup deplyment

--- GradeNest/settings.py
+++ GradeNest/settings.py
@@ -55,9 +55,9 @@ WSGI_APPLICATION = "GradeNest.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        default=os.getenv("DATABASE_URL"),
         conn_max_age=600,
-        ssl_require=False,
+        ssl_require=True
     )
 }
 
